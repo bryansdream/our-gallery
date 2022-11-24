b10fdafa0bdfc902c66b73c08a2e9f126c8d3755
Bryannn <107366548+bryansdream@users.noreply.github.com>
2022-11-24 23:17:23 +0700

    Update revalidate.ts

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,7 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 
-export default async function handler(req, res) {
+export default async function handler(
+  req: NextApiRequest, 
+  res: NextApiResponse
+  ) {
   // Check for secret to confirm this is a valid request
   if (req.query.secret !== process.env.REVALIDATE_SECRET) {
     return res.status(401).json({ message: 'Invalid token' });
