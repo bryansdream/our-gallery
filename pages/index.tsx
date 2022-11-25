d068cfd77f57ba6395304f2f987f6c3ce16a4397
Bryannn <107366548+bryansdream@users.noreply.github.com>
2022-11-25 18:51:19 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { useState } from 'react';
 import { createClient } from '@supabase/supabase-js';
+import Head from "next/head";
 
 const supabaseAdmin = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL || '',
@@ -30,6 +31,10 @@ function cn(...classes: string[]) {
 
 export default function Gallery({ images }: { images: Image[] }) {
   return (
+    <>
+    <Head>
+      <title>The Gallery</title>
+    </Head>
     <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
       <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-1">
       {images.map((image) => (
@@ -37,6 +42,7 @@ export default function Gallery({ images }: { images: Image[] }) {
         ))}
       </div>
     </div>
+    </>
   );
 }
 
